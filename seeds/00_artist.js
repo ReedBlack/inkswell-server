006378a9067baa4e4f4d9c499ef642267987ae56
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-07-18 21:55:20 -0600

    fixed png/jpg bullcockey

--- seeds/00_artist.js
+++ seeds/00_artist.js
@@ -1,17 +1,23 @@
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('artistusers').del()
-    .then(function () {
+  return knex("artistusers")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('artistusers').insert([{
+      return knex("artistusers").insert([
+        {
           id: 1,
           name: "Eliane Lorey",
           email: "Elaine@gmail.com",
           password: "654321",
-          image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine.png",
-          pic_one: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine1.jpg",
-          pic_two: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine2.jpg",
-          pic_three: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine3.jpg",
+          image_link:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine.png",
+          pic_one:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine1.png",
+          pic_two:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine2.png",
+          pic_three:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Elaine3.png",
           shop: "Urban Element"
         },
         {
@@ -19,10 +25,14 @@ exports.seed = function (knex, Promise) {
           name: "Brooke Engleheart",
           email: "Brooke@gmail.com",
           password: "654321",
-          image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke.png",
-          pic_one: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke1.jpg",
-          pic_two: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke2.jpg",
-          pic_three: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke3.jpg",
+          image_link:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke.png",
+          pic_one:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke1.jpg",
+          pic_two:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke2.jpg",
+          pic_three:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Brooke3.jpg",
           shop: "Evoke Tattoos"
         },
         {
@@ -30,10 +40,14 @@ exports.seed = function (knex, Promise) {
           name: "Skyler Espinoza",
           email: "Skyler@gmail.com",
           password: "654321",
-          image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler.png",
-          pic_one: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler1.png",
-          pic_two: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler2.png",
-          pic_three: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler3.png",
+          image_link:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler.png",
+          pic_one:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler1.png",
+          pic_two:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler2.png",
+          pic_three:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/Skyler3.png",
           shop: "Certified Tattoo Studios"
         },
         {
@@ -41,10 +55,14 @@ exports.seed = function (knex, Promise) {
           name: "Rebecca Ashley aka Black Huntress",
           email: "Rebecca@gmail.com",
           password: "654321",
-          image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca.png",
-          pic_one: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca1.png",
-          pic_two: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca2.png",
-          pic_three: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca3.png",
+          image_link:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca.png",
+          pic_one:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca1.png",
+          pic_two:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca2.png",
+          pic_three:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/rebecca3.png",
           shop: "Auspicious Tattoo"
         },
         {
@@ -52,15 +70,19 @@ exports.seed = function (knex, Promise) {
           name: "Fish",
           email: "Fish@gmail.com",
           password: "654321",
-          image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish.png",
-          pic_one: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish1.png",
-          pic_two: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish2.png",
-          pic_three: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish3.png",
+          image_link:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish.png",
+          pic_one:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish1.png",
+          pic_two:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish2.png",
+          pic_three:
+            "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/fish3.png",
           shop: "Th'ink Tank"
         }
-      ])
+      ]);
     })
     .then(() => {
-      return knex.raw("ALTER SEQUENCE artistusers_id_seq RESTART WITH 4;")
-    })
+      return knex.raw("ALTER SEQUENCE artistusers_id_seq RESTART WITH 4;");
+    });
 };
