73e469258446dc52ccc95dd485ecbecdd639a169
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-07-24 16:52:41 -0600

    clearing chat seed

--- seeds/03_chat.js
+++ seeds/03_chat.js
@@ -6,31 +6,19 @@ exports.seed = function (knex, Promise) {
       return knex('chat').insert([{
           chat_id: 1,
           match_id: 2,
-          chat_client: "yo!",
+          chat_client: null,
           chat_artist: null,
         },
         {
           chat_id: 2,
           match_id: 2,
           chat_client: null,
-          chat_artist: "you'z wants tats, brah?",
-        },
-        {
-          chat_id: 3,
-          match_id: 2,
-          chat_client: "yesh",
           chat_artist: null,
-        },
-        {
-          chat_id: 4,
-          match_id: 2,
-          chat_client: null,
-          chat_artist: "I gotchu fam",
-        },
+        }
 
       ])
     })
     .then(() => {
-      return knex.raw("ALTER SEQUENCE chat_chat_id_seq RESTART WITH 5;")
+      return knex.raw("ALTER SEQUENCE chat_chat_id_seq RESTART WITH 3;")
     })
 };
