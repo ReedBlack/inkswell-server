ee1d3ca9955af410849444fa9c8cf80b5d626e8c
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-07-25 11:27:38 -0600

    seed updates

--- seeds/00_client.js
+++ seeds/00_client.js
@@ -8,7 +8,7 @@ exports.seed = function (knex, Promise) {
           client_name: "Reed",
           client_email: "black.reed.c@gmail.com",
           client_password: "123456",
-          client_image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/ClientUsers/Reed.png",
+          client_image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/ClientUsers/Reed.JPG",
           budget: "$650",
           placement: "back shoulder",
           size: "9x9",
@@ -30,7 +30,7 @@ exports.seed = function (knex, Promise) {
           client_name: "Dan",
           client_email: "dan@gmail.com",
           client_password: "123456",
-          client_image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/ClientUsers/Dan.png",
+          client_image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/ClientUsers/Dan.jpg",
           budget: "Dev Money",
           placement: "back",
           size: "whole back",
@@ -60,6 +60,17 @@ exports.seed = function (knex, Promise) {
         },
         {
           client_id: 6,
+          client_name: "Kaitlin",
+          client_email: "Kaitlin@gmail.com",
+          client_password: "123456",
+          client_image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/ClientUsers/Kaitlin.png",
+          budget: "$1500",
+          placement: "full sleeve",
+          size: "medium",
+          description: "I want a full sleeve depicting the epic battle between Godzilla and Mothra over the partially-desctroyed tokyo skyline"
+        },
+        {
+          client_id: 7,
           client_name: "Patrick",
           client_email: "patrick@gmail.com",
           client_password: "123456",
@@ -70,7 +81,7 @@ exports.seed = function (knex, Promise) {
           description: "My satchel is my soul. My lifeblood. I need a true-to-life tattoo of it strapped around my chest and around my back that is so picture-real, you can almost smell the leather"
         },
         {
-          client_id: 7,
+          client_id: 8,
           client_name: "Jose",
           client_email: "jose@gmail.com",
           client_image_link: "https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/inkswell/ClientUsers/Jose.jpeg",
@@ -78,12 +89,12 @@ exports.seed = function (knex, Promise) {
           budget: "$300",
           placement: "the small of my back",
           size: "medium",
-          description: "tribal butterfly emerging from a cocoon, symbolizing my transformation from trudging musician to a beautiful software developing butterfly"
+          description: "I'd like a tribal butterfly emerging from a cocoon, symbolizing my transformation from trudging musician to a beautiful software developing butterfly"
         },
 
       ])
     })
     .then(() => {
-      return knex.raw("ALTER SEQUENCE clientusers_client_id_seq RESTART WITH 7;")
+      return knex.raw("ALTER SEQUENCE clientusers_client_id_seq RESTART WITH 9;")
     })
 };
